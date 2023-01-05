bec84c70e0cd876bfe292c0330c34468db6141f9
OvernightSolo <sterlingtx75@gmail.com>
2023-01-05 16:07:21 -0600

    configured API for city and weather info

---
+++ scripts/forecast.js
@@ -0,0 +1,25 @@
+// This file is for interacting with the APIs
+
+const key = "iyAXl7HahvjcLjFd59qLLrm2DHavXmfy";
+
+// Get weather information
+const getWeather = async id => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// Get city information
+const getCity = async city => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
